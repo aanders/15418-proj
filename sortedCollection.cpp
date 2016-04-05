330ace855552d96f251b926f34bf8bfde0068e0f
Justus <jhibshma@andrew.cmu.edu>
2016-04-05 15:39:38 -0400

    Yolo.

---
+++ sortedCollection.cpp
@@ -0,0 +1,8 @@
+#include "sortedCollection.h"
+
+#ifndef __sortedCollection_cpp__
+#define __sortedCollection_cpp__
+
+template <class T> SortedCollection<T>::SortedCollection() {bob=0;}
+
+#endif
---
+++ sortedCollection.h
@@ -0,0 +1,12 @@
+#ifndef __sortedCollection_h__
+#define __sortedCollection_h__
+
+template <class T> class SortedCollection
+{
+  public:
+  T *bob;
+  
+  SortedCollection();
+};
+
+#endif
