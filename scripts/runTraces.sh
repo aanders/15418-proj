7d03009f4cda82ac6ca14c3b1052eef464c2b794
Aaron Anderson <aaanders@andrew.cmu.edu>
2016-04-21 16:21:12 -0400

    Fill out runTraces script for automated testing

--- scripts/runTraces.sh
+++ scripts/runTraces.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+#
+# Run a collection of traces identified by a
+# subdirectory in the main traces folder
 
-for i in $( ls $1 );
-do
-  ./sortedCollection $1/$i | cut -d \  -f 4 >> $2
+TRACEDIR=traces
+
+function usage() {
+    echo "usage: $0 <tracedir> <outfile>"
+    echo "  <tracedir> : The subdirectory under traces/ contaning the traces to run"
+    echo "  <outfile>  : The file to which the trace timings will be appended"
+    echo ""
+}
+
+if [[ $# < 2 ]] ; then
+    usage
+    exit 1
+fi
+
+if [ ! -d $TRACEDIR/$1 ] ; then
+    echo "error: $TRACEDIR/$1 is not a directory"
+fi
+
+for tracefile in $( ls $TRACEDIR/$1/*.trace ) ; do
+    ./sortedCollection $tracefile | cut -d \  -f 4 >> $2
 done
