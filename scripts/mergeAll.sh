9482f9f6e55023c2a6433a5a46d561306f7ec1af
Aaron Anderson <aaanders@andrew.cmu.edu>
2016-05-07 23:14:51 -0400

    Add a final merge for all output csvs.

--- scripts/mergeAll.sh
+++ scripts/mergeAll.sh
@@ -21,3 +21,7 @@ PREFIXES=$(ls $RESULTDIR | cut -d- -f1-3 | sort -u)
 for p in $PREFIXES; do
     scripts/merge_csv.sh $RESULTDIR/$p* > $OUTPUTDIR/$p.csv
 done
+
+# Also merge all the output files into one
+FILES=( $(ls $RESULTDIR | cut -d- -f1-3 | sed -r 's/([0-9]+)k/\1/' | sort -u -t- -k1,2 -k3n | sed -r 's/$/k.csv/') )
+cat ${FILES[@]/#/$OUTPUTDIR\/} > $OUTPUTDIR/results.csv
